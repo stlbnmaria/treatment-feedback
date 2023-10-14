70dca14e1528911be9cc2114b9604e027ba5dd6c
Maria Stoelben <maria.stlbn@hotmail.de>
2023-10-14 17:59:39 +0200

    changed order and code according to relative file paths from config and renamed main function

--- data_preprocessing/data_preprocess.py
+++ data_preprocessing/data_preprocess.py
@@ -3,6 +3,7 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
+from pathlib import Path
 import pandas as pd
 import string
 import yaml
@@ -13,29 +14,22 @@ nltk.download("stopwords")
 nltk.download("punkt")
 nltk.download("wordnet")
 
-# Read the path from the config.yaml file
-with open(os.path.join(os.getcwd(), "data_preprocessing", "config.yaml")) as f:
-    config = yaml.load(f, Loader=yaml.FullLoader)
 
-
-def preprocess_data(
-    file_path: str,
+def extract_filter_process(
+    file_path: Path,
     diseases: list = [],
     antibodies: list = [],
     treatments: list = [],
-    output_path: str = None,
 ) -> pd.DataFrame:
     """
     Preprocesses the CSV file specified by extracting treatment, disease, and antibody information
     and tokenizing the comments.
 
     Parameters:
-    - file_path (str): Path to the CSV file.
+    - file_path (Path): Path to the CSV file.
     - diseases (list): List of diseases to filter. If empty, no filtering is applied.
     - antibodies (list): List of antibodies to filter. If empty, no filtering is applied.
     - treatments (list): List of treatments to filter. If empty, no filtering is applied.
-     - output_path (str, optional): Path to save the processed dataframe as a CSV file.
-                                   If not provided, the dataframe will not be saved.
 
     Returns:
     - pd.DataFrame: The processed dataframe with added 'treatment', 'disease', 'antibody',
@@ -82,22 +76,41 @@ def preprocess_data(
         ]
     )
 
-    if output_path:
-        dataframe.to_csv(output_path, index=False)
-
     return dataframe
 
 
-def main():
-    df = preprocess_data(
-        config["file_path"],
+def preprocess_data(config_data: Path = Path("config.yaml")):
+    """
+    Initiate preprocessing of data and save the output if specified.
+
+    Parameters:
+    - file_path (Path): Path to config file.
+
+    Returns:
+    - pd.DataFrame: The processed dataframe with added 'treatment', 'disease', 'antibody',
+                    and 'processed_comment' columns.
+    """
+
+    # read the path from the config.yaml file
+    with open(config_data) as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+
+    df = extract_filter_process(
+        file_path=Path(config_data.parent / config["file_path"]),
         diseases=config.get("diseases", []),
         antibodies=config.get("antibodies", []),
         treatments=config.get("treatments", []),
-        output_path=config.get("output_path", None),
     )
     print("-------- Data processing done -------")
 
+    # set the output path of the csv
+    output_path = config.get("output_path", None)
+    if output_path:
+        # save the data to csv if requested
+        df.to_csv(Path(config_data.parent / output_path), index=False)
+
+    return df
+
 
 if __name__ == "__main__":
-    main()
+    preprocess_data()
