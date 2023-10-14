77dd8af54df6eaef0eb69c36200860ba20caf403
DataTom <sch.thomas@live.de>
2023-10-14 22:30:45 +0200

    adding keyword_extraction function using rake_nltk

--- keyword_extraction.py
+++ keyword_extraction.py
@@ -1,5 +1,34 @@
-from data_preprocessing.data_preprocess import main
+# import ast
+# import json
+import pandas as pd
+from rake_nltk import Rake
 
-df = main()
+def keyword_extraction(df: pd.Dataframe):
+    """takes a dataframe as input and extracts the keywords
+       of a column and saves these as a new column
+    
 
-print(df)
+    Args:
+        df (pd.Dataframe): the Dataframe from which to extract the keywords
+
+    Returns:
+         pd.DataFrame: Returns the input dataframe with the added column
+                       with the key words
+    """
+    r = Rake()
+
+    def extract_keywords(text):
+        r.extract_keywords_from_text(text)
+        return r.get_ranked_phrases()
+    
+    df["keywords_comment"] = df["comment"].apply(extract_keywords)
+
+    def extract_keywords_from_list(word_list):
+        text = ' '.join(word_list)
+        r.extract_keywords_from_text(text)
+        return r.get_ranked_phrases()
+
+    df["keywords_processed_column"] = df["processed_comment"].apply(extract_keywords_from_list)
+
+    df.to_csv("data_preprocessing/data/preprocessed.csv")
+    return df
