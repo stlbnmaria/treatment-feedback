cebc2bc988769d9e3806704782cba22137827fae
Steve <stevemoses13@gmail.com>
2023-10-14 16:34:54 +0200

    updated based on review comments

--- data_preprocessing/data_preprocess.py
+++ data_preprocessing/data_preprocess.py
@@ -1,35 +1,41 @@
-import pandas as pd
 import nltk
-import yaml
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
+import pandas as pd
+import string
+import yaml
 
+
+# Download necessary resources for nltk
 nltk.download("stopwords")
 nltk.download("punkt")
 nltk.download("wordnet")
 
+# Read the path from the config.yaml file
+with open("config.yaml") as f:
+    config = yaml.load(f, Loader=yaml.FullLoader)
 
-def preprocess_data(filter: str = "yes") -> pd.DataFrame:
+
+def preprocess_data(file_path: str, 
+                    diseases: list = [], 
+                    antibodies: list = [],
+                    treatments: list = []) -> pd.DataFrame:
     """
-    Preprocesses the CSV file specified in config.yaml by extracting treatment, disease, and antibody information
+    Preprocesses the CSV file specified by extracting treatment, disease, and antibody information
     and tokenizing the comments.
 
     Parameters:
-    - filter (str): If set to "yes", filter the dataframe to only include rows where
-                    the disease is 'Crohn's Disease' or 'Ulcerative Colitis'.
-                    If "no", no filtering is applied.
+    - file_path (str): Path to the CSV file.
+    - diseases (list): List of diseases to filter. If empty, no filtering is applied.
+    - antibodies (list): List of antibodies to filter. If empty, no filtering is applied.
+    - treatments (list): List of treatments to filter. If empty, no filtering is applied.
 
     Returns:
     - pd.DataFrame: The processed dataframe with added 'treatment', 'disease', 'antibody',
                     and 'processed_comment' columns.
     """
-
-    # Read the path from the config.yaml file
-    with open("config.yaml") as f:
-        cfg = yaml.load(f, Loader=yaml.FullLoader)
-    file_path = cfg["file_path"]
-
+    
     # Read the CSV file into a dataframe
     dataframe = pd.read_csv(file_path)
 
@@ -42,11 +48,17 @@ def preprocess_data(filter: str = "yes") -> pd.DataFrame:
     )
     dataframe["antibody"] = dataframe["medication"].str.extract("\(([^)]+)\)")
 
-    # Filter the dataframe based on the disease if filter is set to "yes"
-    if filter == "yes":
-        dataframe = dataframe[
-            dataframe["disease"].isin(["Crohn's Disease", "Ulcerative Colitis"])
-        ]
+    # Filter the dataframe based on the specified diseases
+    if diseases:
+        dataframe = dataframe[dataframe["disease"].isin(diseases)]
+        
+    # Filter the dataframe based on the specified antibodies
+    if antibodies:
+        dataframe = dataframe[dataframe["antibody"].isin(antibodies)]
+        
+    # Filter the dataframe based on the specified treatments
+    if treatments:
+        dataframe = dataframe[dataframe["treatment"].isin(treatments)]
 
     stop_words = set(stopwords.words("english"))
     lemmatizer = WordNetLemmatizer()
@@ -55,9 +67,21 @@ def preprocess_data(filter: str = "yes") -> pd.DataFrame:
     dataframe["processed_comment"] = dataframe["comment"].apply(
         lambda text: [
             lemmatizer.lemmatize(token)
-            for token in word_tokenize(text.lower())
+            for token in word_tokenize(text.lower().strip().translate(str.maketrans('', '', string.punctuation)))
             if token not in stop_words and token.isalpha()
         ]
     )
 
     return dataframe
+
+
+def main():
+    df = preprocess_data(config["file_path"], 
+                         diseases=config.get("diseases", []), 
+                         antibodies=config.get("antibodies", []),
+                         treatments=config.get("treatments", []))
+    print(df.head())
+
+
+if __name__ == "__main__":
+    main()
