05b11072e42458b543707138245c30571e0c584e
KadUsh2001 <joaohfpmelo@gmail.com>
2023-10-17 10:48:54 +0200

    Changes made in order to run sentiment analysis on phrases

--- sentiment_analysis/sentiment_analysis.py
+++ sentiment_analysis/sentiment_analysis.py
@@ -9,116 +9,57 @@ from sklearn.model_selection import train_test_split
 from transformers import pipeline
 from typing import List, Tuple, Union
 
-
-def preprocess_data(path: str, min_df: float, max_df: float) -> Tuple:
+def load_process_sent_data(path: str) -> pd.DataFrame:
     """
-    Loads and preprocesses the data by vectorizing the comments and assigning sentiments.
+    Load and processes the data from Topic extraction.
 
     Parameters:
-    - path (str): The path to the CSV file.
-    - min_df (float): Minimum frequency for words to be considered.
-    - max_df (float): Maximum frequency for words to be considered.
-
+    - path(str): Path to the data which is loaded.
     Returns:
-    - Tuple: A tuple containing the vectorized comments, the true sentiments, and the original dataframe.
+    - pd.DataFrame: Dataframe contained transformed data.
     """
-    # Load data from CSV
+
     df = pd.read_csv(path)
-
-    # Convert the string representation of lists back to actual lists
-    df["processed_comment"] = df["processed_comment"].apply(
-        lambda x: ast.literal_eval(x) if pd.notnull(x) else []
-    )
-
-    # Convert list of tokens into a single string for vectorization
-    comments = df.processed_comment.map(lambda tokens: " ".join(tokens))
-
-    # Assign sentiment based on rating
-    df["true_sentiment"] = np.where(df["rate"] > 5, 1, 0)
-
-    # Create a TF-IDF vectorizer and transform comments
-    vectorizer = TfidfVectorizer(
-        min_df=min_df, max_df=max_df, analyzer="word", ngram_range=(1, 2)
-    )
-    X = vectorizer.fit_transform(comments)
-    y = df["true_sentiment"]
-
-    return X, y, df
-
-
-def train_and_evaluate(
-    X: np.array, y: np.array, df: pd.DataFrame
-) -> Tuple[str, pd.DataFrame]:
-    """
-    Trains a Logistic Regression model, predicts on the entire dataset, and evaluates its performance on the test set.
-
-    Parameters:
-    - X (np.array): The vectorized comments.
-    - y (np.array): The sentiments.
-    - df (pd.DataFrame): The original dataframe.
-
-    Returns:
-    - Tuple: The classification report of the model on the test set and the dataframe with logistic regression predictions for the entire dataset.
-    """
-    # Initialize the Logistic Regression classifier
-    classifier = LogisticRegression(class_weight="balanced", random_state=2023)
-
-    # Split the data into training and test sets
-    X_train, _, y_train, _ = train_test_split(
-        X, y, stratify=y, test_size=0.3, random_state=2023
-    )
-
-    # Train the classifier
-    model = classifier.fit(X_train, y_train)
-
-    # Predict sentiments for the entire dataset
-    yhat = model.predict(X)
-    df["predicted_logreg_sentiment"] = yhat
-
-    # Evaluate the model's performance on the test set
-    _, X_test, _, y_test = train_test_split(
-        X, y, stratify=y, test_size=0.3, random_state=2023
-    )
-    y_test_pred = model.predict(X_test)
-    report = classification_report(y_test, y_test_pred)
-
-    return report, df
-
+    df["category"] = df["category"].apply(
+        lambda x: ast.literal_eval(x) if pd.notnull(x) else [])
+    df["topic"] = df["category"].apply(lambda x: x[0] if len(x) > 0 else "")
+    df = df.drop(["category", "score"], axis=1)
+    df = df[df['phrase'].notna()]
+    #df["phrase"] = df["phrase"].apply(lambda x : str(x))
+    return df
 
 def sentiment_analysis_transformers(df: pd.DataFrame) -> pd.DataFrame:
     """
     Perform sentiment analysis using a pretrained CSV model.
 
     Parameters:
-    - file_path (str): The path to the CSV file.
-
+    - df(pd.Dataframe): Dataframe upon wihch sentiment analysis will be conducted.
     Returns:
     - pd.DataFrame: DataFrame containing original data with added transformer sentiment labels and scores.
     """
 
     # Ensure the 'comment' column exists
-    if "comment" not in df.columns:
-        raise ValueError("The CSV file must contain a 'comment' column.")
+    if "phrase" not in df.columns:
+        raise ValueError("The CSV file must contain a 'phrase' column.")
 
-    comments = df["comment"].tolist()
+    phrases = df["phrase"].tolist()
 
     # Load the classification pipeline
     classifier = pipeline("sentiment-analysis")
 
     # Classify the comments
-    results = classifier(comments)
+    results = classifier(phrases)
 
     # Extract the labels and scores from the results
     df["transformer_sentiment_labels"] = [entry["label"] for entry in results]
-    df["transformer_sentiment_scores"] = [entry["score"] for entry in results]
-
+    df["transformer_sentiment_labels"] = np.where(df["transformer_sentiment_labels"] == "NEGATIVE", 0, 1)
     return df
 
 
 def main():
     # Define the path to the preprocessed data
     path = os.path.join(
-        os.getcwd(), "..", "data_preprocessing", "data", "preprocessed.csv"
+        os.getcwd(), "..", "data_preprocessing", "data", "csv_for_sentiment.csv"
     )
     output_path = os.path.join(
         os.getcwd(),
@@ -129,14 +70,13 @@ def main():
     )
 
     # Preprocess data
-    X, y, df = preprocess_data(path, min_df=0.01, max_df=0.99)
+    df = load_process_sent_data(path)
 
-    # Train the model, predict, and evaluate
-    report, df_with_predictions = train_and_evaluate(X, y, df)
+    #   Use pre-trained model to predict sentiment
+    df = sentiment_analysis_transformers(df)
 
-    df_with_predictions = sentiment_analysis_transformers(df_with_predictions)
-
-    df_with_predictions.to_csv(output_path)
+    #df_with_predictions.to_csv(output_path)
+    df.to_csv(output_path)
 
     print("------- Sentiment Analysis Completed -------")
 
