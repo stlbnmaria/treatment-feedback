2647ffc6d188a3de82bda0135aa8c04aa86e9153
Maria Stoelben <maria.stlbn@hotmail.de>
2023-10-17 14:43:09 +0200

    carved out helper function to extract key phrases

--- phrase_modeling/phrase_extraction.py
+++ phrase_modeling/phrase_extraction.py
@@ -1,16 +1,31 @@
 import pandas as pd
 from rake_nltk import Rake
+from typing import List
 
 
-def phrase_extraction(df: pd.DataFrame, column_name_comment: str = "comment"):
-    """takes a dataframe as input and extracts the keywords
+def extract_keyphrase(text: str, r: Rake) -> List[str]:
+    """extracts key phrases from a text input (in our case the comments)
+
+    Args:
+        text (str): the text that from which phrases should be extracted
+        r (Rank): Rake object to extract phrases
+
+    Returns:
+        List of the key phrases from the text
+    """
+    r.extract_keywords_from_text(text)
+    return r.get_ranked_phrases()
+
+
+def phrase_extraction(
+    df: pd.DataFrame, column_name_comment: str = "comment"
+) -> pd.DataFrame:
+    """takes a dataframe as input and extracts the key phrases
        of a column and saves these as a new column
-
 
     Args:
         df (pd.Dataframe): the Dataframe from which to extract the keywords
         column_name_comment (str): the column which has the untreated comments
-        column_name_processed (str): the column which has the proccessed comments
 
     Returns:
          pd.DataFrame: Returns the input dataframe with two added columns
@@ -19,21 +34,7 @@ def phrase_extraction(df: pd.DataFrame, column_name_comment: str = "comment"):
     # setting min length to 4 to extract small phrases
     r = Rake(min_length=4, max_length=10)
 
-    # function for extracting the phrases
-    def extract_keyphrase(text):
-        """extracts key phrases from a text input (in our case the comments)
-
-        Args:
-            text (str): the text that from which phrases should be extracted
-
-        Returns:
-            r: the key phrases from the text
-        """
-        r.extract_keywords_from_text(text)
-        return r.get_ranked_phrases()
-
     # applies phrase extraction to chosen comment and creates new column "phrases"
-    df["phrases"] = df[column_name_comment].apply(extract_keyphrase)
+    df["phrases"] = df[column_name_comment].apply(lambda x: extract_keyphrase(x, r))
 
-    # df.to_csv("data_preprocessing/data/preprocessed.csv")
     return df
