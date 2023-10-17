e0b514c6667dfa236a2f39ea4a5f1414f9a4558e
DataTom <sch.thomas@live.de>
2023-10-17 12:10:47 +0200

    cleaning up branch

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from pathlib import Path
 
 from data_preprocessing.data_preprocess import preprocess_data
 from keyword_extraction import keyword_extraction
-from phrase_extraction import phrase_extraction
+from phrase_modeling.phrase_classification import phrase_classification
+from phrase_modeling.phrase_extraction import phrase_extraction
 
 
 @click.command()
@@ -17,6 +18,9 @@ def main(config_data: Path):
     df = preprocess_data(config_data)
     df = keyword_extraction(df)
     df = phrase_extraction(df)
+
+    df_phrase = phrase_classification(df)
+
 
 if __name__ == "__main__":
     main()
