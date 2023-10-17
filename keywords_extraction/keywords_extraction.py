7e76e821da1b76597bba1e7116714c2bcf2b72f0
Liza Barysheva <baryshevaes.main@gmail.com>
2023-10-17 19:19:57 +0200

    Fix lemmatization problem

--- keywords_extraction/keywords_extraction.py
+++ keywords_extraction/keywords_extraction.py
@@ -1,9 +1,87 @@
 from ast import literal_eval
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
 import pandas as pd
 from pathlib import Path
 from rake_nltk import Rake
+import string
 import yaml
-from data_preprocessing.data_preprocess import lemmatize_case
+
+
+def kewords_lemmatization(value):
+    stop_words = set(stopwords.words("english"))
+    lemmatizer = WordNetLemmatizer()
+
+    lemmatized_tokens = [
+        lemmatizer.lemmatize(token)
+        for token in word_tokenize(
+            value.lower().strip().translate(str.maketrans("", "", string.punctuation))
+        )
+        if token not in stop_words and token.isalpha()
+    ]
+    return " ".join(
+        lemmatized_tokens
+    )  # Join the lemmatized tokens into a single string
+
+
+def lemmatize_case(value):
+    """
+    Lemmatizes a given text string by tokenizing it, converting to lowercase,
+    removing punctuation, and lemmatizing non-stopword, alphabetical tokens.
+
+    Parameters:
+    - value (str): The input text string to be lemmatized.
+
+    Returns:
+    - list: A list of lemmatized tokens from the input text, excluding stop words and non-alphabetical tokens.
+          If the input value is None or NaN, an empty list is returned.
+    """
+
+    stop_words = set(stopwords.words("english"))
+    lemmatizer = WordNetLemmatizer()
+
+    if pd.notna(value):
+        return [
+            lemmatizer.lemmatize(token)
+            for token in word_tokenize(
+                value.lower()
+                .strip()
+                .translate(str.maketrans("", "", string.punctuation))
+            )
+            if token not in stop_words and token.isalpha()
+        ]
+    else:
+        return []
+
+
+def remove_disease_terms(row):
+    """
+    Filter out words from a processed comment that appear in the names of the treatment,
+    disease and anti-body for this disease.
+
+    Parameters:
+    - row (pandas.Series): A pandas Series containing columns.
+
+    Returns:
+    - list: A list of words from 'processed_comment' that are not found in any of the lemmatized sets
+    ('lemmatized_disease', 'lemmatized_treatment', 'lemmatized_antibody').
+    """
+
+    return [
+        keyword
+        for keyword in row["keywords_comment"]
+        if all(
+            word
+            not in [
+                row["lemmatized_disease"],
+                row["lemmatized_treatment"],
+                row["lemmatized_antibody"],
+                "uc",
+            ]
+            for word in keyword.split()
+        )
+    ]
 
 
 def extract_keywords(text):
@@ -32,14 +110,19 @@ def extract_keywords_from_comments(
 
     df["keywords_comment"] = df["comment"].apply(extract_keywords)
 
+    # change column to list of strings instead of whole string
+    df["keywords_comment"] = df.keywords_comment.apply(lambda x: literal_eval(str(x)))
+
     df["keywords_comment"] = df["keywords_comment"].apply(
-        lambda keywords: [lemmatize_case(keyword) for keyword in keywords]
+        lambda keywords: [kewords_lemmatization(keyword) for keyword in keywords]
     )
 
-    # Change column to list of strings instead of whole string
-    df["keywords_comment"] = df.processed_comment.apply(lambda x: literal_eval(str(x)))
+    # Convert to lowercase, tokenize, remove stopwords, and apply lemmatization
+    df["lemmatized_disease"] = df["disease"].apply(lemmatize_case)
+    df["lemmatized_antibody"] = df["antibody"].apply(lemmatize_case)
+    df["lemmatized_treatment"] = df["treatment"].apply(lemmatize_case)
 
-    print("-------- Key words extraction done -------")
+    df["keywords_comment"] = df.apply(remove_disease_terms, axis=1)
 
     # Set the output path of the csv
     output_path = config.get("keywords_output_file_path", None)
